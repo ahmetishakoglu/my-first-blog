0ceea67506d6e81dfee3db734a82d554119a7c32
yilmaz.ishakoglu <a.yilmaz.ishakoglu@gmail.com>
2019-07-10 16:20:17 +0300

    Dummy

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -11,10 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
-application = DjangoWhiteNoise(application)
-
-application = WhiteNoise(application, root='./blog/static')
+from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
